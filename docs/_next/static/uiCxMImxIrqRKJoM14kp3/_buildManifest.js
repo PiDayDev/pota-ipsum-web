ffde189cea3f7b6052b889a20be59e1780033f58
Damiano Salvi <salvi@intre.it>
2023-03-28 16:37:42 +0200

    💚 Try CI (5)

--- docs/_next/static/uiCxMImxIrqRKJoM14kp3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/402-a297e6b8ea0b6771.js","static/css/bf02be9c4ad5981e.css","static/chunks/pages/index-ddf52c8aa0c8d30b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
