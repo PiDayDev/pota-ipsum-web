a408377b9bcda7ebe61b5d1e7056b850b6f74e29
Damiano Salvi <salvi@intre.it>
2023-04-17 17:30:59 +0200

    ✨ Add generation options

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { useState } from 'react';
-import { PotaIpsum } from 'pota-ipsum';
+import {useState} from 'react';
+import {PotaIpsum} from 'pota-ipsum';
 
 const pota = new PotaIpsum({
     sentencesPerParagraph: {
@@ -16,11 +16,25 @@ const pota = new PotaIpsum({
 
 export default function Home() {
     const [text, setText] = useState<string>('');
+    const [count, setCount] = useState(5);
+    const [what, setWhat] = useState('paras');
 
     const generate = () => {
-        let s = pota.generateParagraphs(3);
-        setText(s.replace(/\n/g, '\n\n'));
-        console.log(s)
+        const amount = count || 3
+        let text = '';
+        switch (what) {
+            case "paras":
+                text = pota.generateParagraphs(amount);
+                break;
+            case "sents":
+                text = pota.generateSentences(amount);
+                break;
+            case "words":
+                text = pota.generateWords(amount);
+                break;
+        }
+        setText(text.replace(/\n/g, '\n\n'));
+        console.log(what,count,text)
     }
 
     return (
@@ -32,7 +46,34 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className={styles.main}>
-                <div className={styles.description} onClick={generate}>Generate</div>
+                <div className={styles.description}>
+                    <div className={styles.action} onClick={generate}>Generate</div>
+                    <div/>
+                    <input className={styles.input} type="text" name="amount" id="amount"
+                           defaultValue={count}
+                           onChange={v => setCount(+v.target.value)}/>
+                    <div className={styles.label}>
+                        <label htmlFor="paras">
+                            <input type="radio" name="what" value="paras" id="paras"
+                                   defaultChecked={"paras" === what}
+                                   onChange={() => setWhat("paras")}
+                            /> paragraphs
+                        </label>
+                        <label htmlFor="sents">
+                            <input type="radio" name="what" value="sents" id="sents"
+                                   defaultChecked={"sents" === what}
+                                   onChange={() => setWhat("sents")}
+                            /> sentences
+                        </label>
+                        <label htmlFor="words">
+                            <input type="radio" name="what" value="words" id="words"
+                                   defaultChecked={"words" === what}
+                                   onChange={() => setWhat("words")}
+                            /> words
+                        </label>
+                    </div>
+                </div>
+
                 <textarea className={styles.center} readOnly value={text}/>
             </main>
         </>
