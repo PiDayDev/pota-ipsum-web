564c3d6b7d6f13be4b7249978faf516e910b1571
Damiano Salvi <salvi@intre.it>
2026-02-16 15:48:16 +0100

    fix: add favicon link to document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon per tutte le pagine */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
